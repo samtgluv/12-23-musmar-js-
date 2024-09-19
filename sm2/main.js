7b9f345b6d54089faeb87b9dd312185a411fce43
марьям <samtgvvluv@gmail.com>
2024-09-19 11:09:00 +0300

    test

---
+++ sm2/main.js
@@ -0,0 +1,17 @@
+let name = prompt('Как тебя зовут?');
+    alert(`Тебя зовут ${name}!`); 
+let age = prompt('Сколько тебе лет?', 100); 
+    if (age < 20) {
+        alert( 'Тебе же меньше 20!' );
+      } 
+    if (age >= 20) {
+        for ( let i = 0; i < 5; i++){
+            alert('Привет')
+        }
+      } 
+let js = confirm("Хочешь учить js?");
+    if (js) {
+        alert('супер')}
+    else {
+         alert('не супер((')}
+      
